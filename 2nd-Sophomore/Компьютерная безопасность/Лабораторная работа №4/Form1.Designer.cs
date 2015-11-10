1af374a188808d785b2294c43edd493553baccc8
Michael <ashtazar@gmail.com>
2015-11-10 12:35:44 +0700

    Added CS Lab 4(Caesar - Peter)

---
+++ 2nd-Sophomore/Компьютерная безопасность/Лабораторная работа №4/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CS_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnEncodeCaesar = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnEncodePetrI = new System.Windows.Forms.Button();
+            this.btnDecodeCaesar = new System.Windows.Forms.Button();
+            this.btnDecodePetrI = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbProcent = new System.Windows.Forms.Label();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbInput.Location = new System.Drawing.Point(12, 12);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(622, 194);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.Text = "Исходное сообщение";
+            this.tbInput.Click += new System.EventHandler(this.tbInput_Click);
+            // 
+            // btnEncodeCaesar
+            // 
+            this.btnEncodeCaesar.BackColor = System.Drawing.Color.White;
+            this.btnEncodeCaesar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEncodeCaesar.Location = new System.Drawing.Point(12, 212);
+            this.btnEncodeCaesar.Name = "btnEncodeCaesar";
+            this.btnEncodeCaesar.Size = new System.Drawing.Size(93, 37);
+            this.btnEncodeCaesar.TabIndex = 1;
+            this.btnEncodeCaesar.Text = "Зашифровать кодом Цезаря";
+            this.btnEncodeCaesar.UseVisualStyleBackColor = false;
+            this.btnEncodeCaesar.Click += new System.EventHandler(this.btnEncodeCaesar_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbResult.Location = new System.Drawing.Point(12, 255);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(622, 194);
+            this.tbResult.TabIndex = 2;
+            this.tbResult.Text = "Зашифрованное сообщение";
+            // 
+            // btnEncodePetrI
+            // 
+            this.btnEncodePetrI.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEncodePetrI.Location = new System.Drawing.Point(442, 212);
+            this.btnEncodePetrI.Name = "btnEncodePetrI";
+            this.btnEncodePetrI.Size = new System.Drawing.Size(93, 37);
+            this.btnEncodePetrI.TabIndex = 3;
+            this.btnEncodePetrI.Text = "Зашифровать кодом Петра I";
+            this.btnEncodePetrI.UseVisualStyleBackColor = true;
+            this.btnEncodePetrI.Click += new System.EventHandler(this.btnEncodePetrI_Click);
+            // 
+            // btnDecodeCaesar
+            // 
+            this.btnDecodeCaesar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnDecodeCaesar.Location = new System.Drawing.Point(111, 212);
+            this.btnDecodeCaesar.Name = "btnDecodeCaesar";
+            this.btnDecodeCaesar.Size = new System.Drawing.Size(93, 37);
+            this.btnDecodeCaesar.TabIndex = 4;
+            this.btnDecodeCaesar.Text = "Расшифровать код Цезаря";
+            this.btnDecodeCaesar.UseVisualStyleBackColor = true;
+            this.btnDecodeCaesar.Click += new System.EventHandler(this.btnDecodeCaesar_Click);
+            // 
+            // btnDecodePetrI
+            // 
+            this.btnDecodePetrI.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnDecodePetrI.Location = new System.Drawing.Point(541, 212);
+            this.btnDecodePetrI.Name = "btnDecodePetrI";
+            this.btnDecodePetrI.Size = new System.Drawing.Size(93, 37);
+            this.btnDecodePetrI.TabIndex = 5;
+            this.btnDecodePetrI.Text = "Расшифровать код Петра I";
+            this.btnDecodePetrI.UseVisualStyleBackColor = true;
+            this.btnDecodePetrI.Click += new System.EventHandler(this.btnDecodePetrI_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вероятность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbProcent
+            // 
+            this.lbProcent.AutoSize = true;
+            this.lbProcent.Location = new System.Drawing.Point(368, 228);
+            this.lbProcent.Name = "lbProcent";
+            this.lbProcent.Size = new System.Drawing.Size(0, 13);
+            this.lbProcent.TabIndex = 8;
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(652, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(112, 437);
+            this.listView.TabIndex = 9;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(776, 461);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.lbProcent);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDecodePetrI);
+            this.Controls.Add(this.btnDecodeCaesar);
+            this.Controls.Add(this.btnEncodePetrI);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btnEncodeCaesar);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnEncodeCaesar;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnEncodePetrI;
+        private System.Windows.Forms.Button btnDecodeCaesar;
+        private System.Windows.Forms.Button btnDecodePetrI;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbProcent;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ 2nd-Sophomore/Компьютерная безопасность/Лабораторная работа №4/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CS_4
+{
+    public partial class Form1 : Form
+    {
+        char[] alpharus = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+                            'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ',
+                            'Ы', 'Ь', 'Э', 'Ю', 'Я', ' ' };
+
+        char[] alpharusMoved = { 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+                                 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч',
+                                 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', ' ', 'А', 'Б', 'В' };
+
+        double[] arrayStat = new double[34];
+
+        double[] occurance = new double[34];
+
+        double[] occuranceBase =
+        {
+            0.064, 0.015, 0.039, 0.014, 0.026, 0.074, 0.074, 0.008, 0.015, 0.064, 0.01, 0.029, 0.036, 0.026,
+            0.056, 0.095, 0.024, 0.041, 0.047, 0.056, 0.021, 0.002, 0.009, 0.004, 0.013, 0.006, 0.003, 0.015,
+            0.016, 0.015, 0.003, 0.007, 0.019, 0.124
+        };
+
+        readonly string[] peterKey = { "ме", "ли", "ко", "ин", "зе", "жу", "ню", "оы", "пы", "ра", "су", "ти",
+                                       "уф", "хм", "от", "ца", "чу", "ше", "ам", "эм", "яъ", "от", "нь", "щъ",
+                                       "юз", "яз", "фу", "бе", "ва", "гу", "дм", "го", "вй" };
+
+        readonly char[] alphaeng = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                                     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                     'Y', 'Z', 'A', 'B', 'C' };
+
+        private char[] inputMain;
+        private char[] outputMain;
+
+        double[] occuranceBackUp = new double[34];
+        double[] occuranceBaseBackUp = new double[34];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Шифровка текста кодом Цезаря
+        private void btnEncodeCaesar_Click(object sender, EventArgs e)
+        {
+            inputMain = tbInput.Text.ToUpper().ToCharArray();
+            CaesarEncode(alpharus, alpharusMoved);
+        }
+
+        private void CaesarEncode(IReadOnlyList<char> alphabet, char[] alphabetMoved)
+        {
+            var input = tbInput.Text.ToUpper();
+            var output = "";
+
+            foreach (var t in input)
+            {
+                for (var j = 0; j < alphabet.Count(); ++j)
+                {
+                    if (t != alphabet[j]) continue;
+                    output += alphabetMoved[j];
+                    break;
+                }
+            }
+
+            tbResult.Text = output;
+        }
+        #endregion
+
+        #region Расшифровка кода Цезаря
+        private void btnDecodeCaesar_Click(object sender, EventArgs e)
+        {
+            CaesarDecode(alpharus, alpharusMoved);
+        }
+        
+        private void CaesarDecode(IReadOnlyList<char> alphabet, char[] alphabetMoved)
+        {
+            var input = tbInput.Text;
+            var output = "";
+
+            foreach (var t in input)
+            {
+                for (var j = 0; j < alphabetMoved.Count(); ++j)
+                {
+                    if (t != alphabetMoved[j]) continue;
+                    output += alphabet[j];
+                    break;
+                }
+            }
+
+            tbResult.Text = output;
+        }
+        #endregion
+
+        #region Шифровка текста кодом Петра Первого
+        private void btnEncodePetrI_Click(object sender, EventArgs e)
+        {
+            inputMain = tbInput.Text.ToUpper().ToCharArray();
+            PetrIEncode(alpharus);           
+        }
+
+        private void PetrIEncode(IReadOnlyList<char> alphabet)
+        {
+            var input = tbInput.Text.ToUpper();
+            var output = "";
+
+            foreach (var t in input)
+            {
+                if (t == ' ')
+                {
+                    output += " ";
+                }
+                else
+                    for (var j = 0; j < alphabet.Count; ++j)
+                    {
+                        if (t != alphabet[j]) continue;
+                        output += peterKey[j].ToUpper();
+                        break;
+                    }
+            }
+
+            tbResult.Text = output;
+        }
+        #endregion
+
+        #region Дешифровка кода Петра Первого
+        private void btnDecodePetrI_Click(object sender, EventArgs e)
+        {
+            PetrIDecode(alpharus);           
+        }
+
+        private void PetrIDecode(char[] alphabet)
+        {
+            var input = tbInput.Text;
+            var output = "";
+
+            for (var i = 0; i < input.Length - 1; ++i)
+            {
+                if (input[i] == ' ')
+                {
+                    output += " ";
+                }
+                else
+                {
+                    var tmp = input[i].ToString() + input[i + 1].ToString();
+                    for (var j = 0; j < peterKey.Length; ++j)
+                    {
+                        if (tmp == peterKey[j])
+                        {
+                            output += alphabet[j];
+                        }
+                    }
+                    ++i;
+                }
+            }
+
+            tbResult.Text = output.ToLower();
+        }
+        #endregion
+
+        private void tbInput_Click(object sender, EventArgs e)
+        {
+            //tbInput.Text = "";
+        }
+
+        private void Statistics()
+        {
+            tbResult.Text = "";
+            var input = tbInput.Text.ToUpper();
+            for (var i = 0; i < alpharus.Length; ++i)
+            {
+                foreach (var t in input)
+                {
+                    if (alpharus[i] == t)
+                    {
+                        ++arrayStat[i];
+                    }
+                }
+            }
+
+            for (var i = 0; i < arrayStat.Length; ++i)
+            {
+                occurance[i] = Math.Round(arrayStat[i]/input.Length, 3);
+            }
+
+            foreach (var t in occurance)
+            {
+                tbResult.Text += t.ToString() + " ";
+            }
+
+            for (var i = 0; i < occuranceBase.Length; i++)
+            {
+                occuranceBaseBackUp[i] = occuranceBase[i];
+            }
+
+            for (var i = 0; i < occurance.Length; i++)
+            {
+                occuranceBackUp[i] = occurance[i];
+            }
+        }
+
+        private void CompareOccurance()
+        {
+            tbResult.Text = "";
+            var input = tbInput.Text.ToCharArray();
+            var output = tbInput.Text.ToCharArray();
+
+            for (var t = 0; t <= 34; ++t)
+            {
+                var curMax = occuranceBackUp.Max();
+                var curMaxInd = Array.IndexOf(occuranceBackUp, curMax);
+
+                var curMaxBase = occuranceBaseBackUp.Max();
+                var curMaxBaseInd = Array.IndexOf(occuranceBaseBackUp, curMaxBase);
+
+                for (var i = 0; i < input.Length; ++i)
+                    if (input[i] == alpharus[curMaxInd])
+                        output[i] = alpharus[curMaxBaseInd];
+
+                occuranceBackUp[curMaxInd] = 0;
+                occuranceBaseBackUp[curMaxBaseInd] = 0;
+            }
+
+            foreach (var t in output)
+            {
+                tbResult.Text += t;
+            }
+            outputMain = tbResult.Text.ToCharArray();
+        }
+
+        private double CalculateProcent(char[] input, char[] output)
+        {
+            double count = 0;
+            for (var i = 0; i < input.Length; ++i)
+            {
+                if (input[i] == output[i]) ++count;
+            }
+            return Math.Round(count / input.Length, 6);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Statistics();
+            CompareOccurance();
+            lbProcent.Text = CalculateProcent(inputMain, outputMain).ToString();
+            listView.Columns.Add("O", 40);
+            listView.Columns.Add("B", 40);
+            for (var i=0; i < occurance.Length; ++i)
+            {
+                listView.Items.Add(new ListViewItem(
+                    new string[]
+                    {
+                        occurance[i].ToString(),
+                        occuranceBase[i].ToString()
+                    }));
+            }
+        }
+    }
+}
