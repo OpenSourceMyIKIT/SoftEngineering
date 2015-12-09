1f4096241c9bf5950f0832bea099cdb94f05eed0
Michael <ashtazar@gmail.com>
2015-12-09 15:31:26 +0700

    Study Progress initial commit

---
+++ 2nd-Sophomore/Командный курсовой проект/StudyProgress/Model/Term.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Model
+{
+    public class Term
+    {
+        public ObservableCollection<Subject> Subjects { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ 2nd-Sophomore/Командный курсовой проект/StudyProgress/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Navigation;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
