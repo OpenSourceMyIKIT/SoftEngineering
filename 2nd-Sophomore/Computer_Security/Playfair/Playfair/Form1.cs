94a5af13b36aed6f7c6c1b7ecea59b32b51ebe82
Michael <ashtazar@gmail.com>
2015-12-13 19:53:31 +0700

    Few minor changes

--- 2nd-Sophomore/Computer_Security/Playfair/Playfair/Form1.cs
+++ 2nd-Sophomore/Computer_Security/Playfair/Playfair/Form1.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Playfair
 {
     public partial class Form1 : Form
     {
-        // Матрица которая используется для заполнения ключом,
-        // а затем разбивается в двумерную матрицу
+        /// Матрица которая используется для заполнения ключом,
+        /// а затем разбивается в двумерную матрицу
         private char[] _initialMatrix = new char[25];
-        private char[,] _keyMatrix = new char[5,5];
+        private char[,] _keyMatrix = new char[5, 5];
 
-        // Ключ, который задает пользователь
+        /// Ключ, который задает пользователь
         private string _key;
-        private char[] _alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                                     'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-                                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        private readonly char[] _alphabet =
+        {
+            'a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i', 'k',
+            'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z'
+        };
 
         public Form1()
         {
@@ -35,57 +34,53 @@ namespace Playfair
             FillKeyMatrix();
         }
 
-        // Заполняет начальную матрицу ключом (без повторений)
+        /// Заполняет начальную матрицу ключом (без повторений)
         private void FillInitialMatrix()
         {
             _key = tbKey.Text;
-            int j = 0;
-            for (int i = 0; i < _key.Length; ++i)
+            var j = 0;
+            for (var i = 0; i < _key.Length; ++i)
             {
-                if (!(CheckMatches(i, _key[i])))
-                {
-                    _initialMatrix[j] = _key[i];
-                    j++;
-                }
+                if (CheckMatches(i, _key[i])) continue;
+                _initialMatrix[j] = _key[i];
+                j++;
             }
         }
 
-        // Проверяет совпадение заносимого символа со всеми уже записанными символами
+        /// Проверяет совпадение заносимого символа со всеми уже записанными символами
         private bool CheckMatches(int index, char c)
         {
             for (var i = 0; i < index; ++i)
-            {              
-               if (c == _initialMatrix[i]) return true;              
+            {
+                if (c == _initialMatrix[i]) return true;
             }
             return false;
         }
 
-        // Добавляет к начальной матрице алфавит (без повторений)
+        /// Добавляет к начальной матрице алфавит (без повторений)
         private void FillAlphabetMatrix()
         {
-            int j = 0;
-            for (int i = 0; i < _alphabet.Length; ++i)
+            var j = 0;
+            for (var i = 0; i < _alphabet.Length; ++i)
             {
-                if (_initialMatrix[i] == '\0')
-                { 
-                    if (!(CheckMatches(i, _alphabet[j])))
-                    {
-                        _initialMatrix[i] = _alphabet[j];
-                        j++;
-                        continue;
-                    }                    
+                if (_initialMatrix[i] != '\0') continue;
+                if (!(CheckMatches(i, _alphabet[j])))
+                {
+                    _initialMatrix[i] = _alphabet[j];
                     j++;
-                    i--;
+                    continue;
                 }
+                j++;
+                i--;
             }
         }
 
-        // Разбивает одномерную матрицу на двумерную
+        /// Разбивает одномерную матрицу на двумерную
         private void FillKeyMatrix()
         {
-            int k = 0;
-            for (int i = 0; i < 5; ++i)
-                for (int j = 0; j < 5; ++j)
+            var k = 0;
+            for (var i = 0; i < 5; ++i)
+                for (var j = 0; j < 5; ++j)
                 {
                     _keyMatrix[i, j] = _initialMatrix[k];
                     ++k;
--- 2nd-Sophomore/Computer_Security/Playfair/Playfair/Properties/Resources.Designer.cs
+++ 2nd-Sophomore/Computer_Security/Playfair/Playfair/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace Playfair.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Playfair.Properties.Resources", typeof(Resources).Assembly);
+                    var temp = new global::System.Resources.ResourceManager("Playfair.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
