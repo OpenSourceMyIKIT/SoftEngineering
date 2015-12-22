6905bc712d3795062c78bc46025ebb9114435e14
Michael <ashtazar@gmail.com>
2015-12-22 21:08:09 +0700

    Cleaned up Study Progress a bit

--- 2nd-Sophomore/Командный курсовой проект/StudyProgress/Model/Properties/AssemblyInfo.cs
+++ 2nd-Sophomore/Командный курсовой проект/StudyProgress/Model/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Model")]
+[assembly: AssemblyTitle("SharedModel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Model")]
+[assembly: AssemblyProduct("SharedModel")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
